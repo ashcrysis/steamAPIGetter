8710bce96bc035385723430aa3ba58a42fd4f954
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2023-10-18 19:01:08 -0300

    Revert "v-008 : Translating Code"
    This reverts commit 88433a6fbfe07568ba5a4c5b2f37058a70bfb3ec.

--- scripts/catchInterface.py
+++ scripts/catchInterface.py
@@ -9,206 +9,210 @@ import webbrowser
 from tkinter import ttk
 import os
 
-def open_linkedin():
+def abrir_linkedin():
     webbrowser.open("https://www.linkedin.com/in/asher-gabriela/")
 
-def open_github():
+def abrir_github():
     webbrowser.open("https://github.com/ashcrysis")
 
-def create_colored_circle(color, size=(15, 15)):
-    image = Image.new("RGBA", size, (255, 255, 255, 0))
-    draw = ImageDraw.Draw(image)
-    draw.ellipse([(0, 0), size], fill=color)
-    return ImageTk.PhotoImage(image)
+def criar_circulo_cor(cor, tamanho=(15, 15)):
+    imagem = Image.new("RGBA", tamanho, (255, 255, 255, 0))
+    desenho = ImageDraw.Draw(imagem)
+    desenho.ellipse([(0, 0), tamanho], fill=cor)
+    return ImageTk.PhotoImage(imagem)
 
-def map_persona_state(numeric_state):
-    if numeric_state == 0:
+def mapear_persona_state(estado_numerico):
+    if estado_numerico == 0:
         return "Offline"
-    elif numeric_state == 1:
+    elif estado_numerico == 1:
         return "Online"
-    elif numeric_state == 2:
+    elif estado_numerico == 2:
         return "Busy"
-    elif numeric_state == 3:
+    elif estado_numerico == 3:
         return "Away"
-    elif numeric_state == 4:
+    elif estado_numerico == 4:
         return "Snooze"
-    elif numeric_state == 5:
+    elif estado_numerico == 5:
         return "Looking to Trade"
-    elif numeric_state == 6:
+    elif estado_numerico == 6:
         return "Looking to Play"
     else:
-        return "Unknown State"
+        return "Estado Desconhecido"
 
-def get_game_list(steam_id, api_key):
+
+def obter_lista_jogos(steam_id, api_key):
     url = f'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={steam_id}&format=json'
     
     try:
-        response = requests.get(url)
-        data = response.json()
+        resposta = requests.get(url)
+        dados = resposta.json()
         
-        if 'response' in data and 'games' in data['response']:
-            games = data['response']['games']
-            return games
+        if 'response' in dados and 'games' in dados['response']:
+            jogos = dados['response']['games']
+            return jogos
         else:
-            print('Error retrieving game list.')
+            print('Erro ao obter lista de jogos.')
             return None
     
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Erro: {e}')
         return None
 
 @functools.lru_cache(maxsize=None)
-def get_game_name_and_image(appid, api_key):
+def obter_nome_e_imagem_jogo(appid, api_key):
     url_info = f'http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key={api_key}&appid={appid}'
 
     try:
-        response_info = requests.get(url_info)
-        data_info = response_info.json()
+        resposta_info = requests.get(url_info)
+        dados_info = resposta_info.json()
 
-        if 'game' in data_info and 'gameName' in data_info['game']:
-            game_name = data_info['game']['gameName']
+        if 'game' in dados_info and 'gameName' in dados_info['game']:
+            nome_do_jogo = dados_info['game']['gameName']
 
-            # Build the game image URL
+            # Construir URL da imagem do jogo
             img_icon_url = f"http://media.steampowered.com/steamcommunity/public/images/apps/{appid}/{appid}.jpg"
 
-            return game_name, img_icon_url
+            return nome_do_jogo, img_icon_url
         else:
-            print('Error retrieving game information.')
+            print('Erro ao obter informações do jogo.')
             return None, None
 
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Erro: {e}')
         return None, None
 
-def get_player_info(steam_id, api_key):
+def obter_informacoes_jogador(steam_id, api_key):
     url = f'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={api_key}&steamids={steam_id}'
 
     try:
-        response = requests.get(url)
-        data = response.json()
+        resposta = requests.get(url)
+        dados = resposta.json()
 
-        if 'response' in data and 'players' in data['response']:
-            player = data['response']['players'][0]
-            personaname = player.get('personaname', 'Name not available')
-            personastate = player.get('personastate', 'State not available')
-            avatar_url = player.get('avatarfull', None)
+        if 'response' in dados and 'players' in dados['response']:
+            jogador = dados['response']['players'][0]
+            personaname = jogador.get('personaname', 'Nome não disponível')
+            personastate = jogador.get('personastate', 'Estado não disponível')
+            avatar_url = jogador.get('avatarfull', None)
 
-            # Add information about the current game and the last played game
-            current_game = player.get('gameextrainfo', 'Not currently playing')
-            last_game = player.get('last_app', 'No game history')
+            # Adicione informações sobre o jogo atual e o último jogo jogado
+            jogo_atual = jogador.get('gameextrainfo', 'Não está jogando no momento')
+            ultimo_jogo = jogador.get('last_app', 'Não possui histórico de jogos')
 
-            return personaname, personastate, avatar_url, current_game, last_game
+            return personaname, personastate, avatar_url, jogo_atual, ultimo_jogo
         else:
-            print('Error retrieving player information.')
+            print('Erro ao obter informações do jogador.')
             return None, None, None, None, None
 
     except Exception as e:
-        print(f'Error: {e}')
+        print(f'Erro: {e}')
         return None, None, None, None, None
 
-def create_main_screen():
-    def get_information():
+
+def criar_tela():
+    def obter_informacoes():
        
         steam_id = entry_steam_id.get()
-        api_key = 'DE8CA38B8BF3145D2D3DD61C192216E8'
+        api_key = 'YOUR_API_KEY'
 
-        personaname, personastate, avatar_url, current_game, last_game = get_player_info(steam_id, api_key)
-        game_list = get_game_list(steam_id, api_key)
+        personaname, personastate, avatar_url, jogo_atual, ultimo_jogo = obter_informacoes_jogador(steam_id, api_key)
+        lista_jogos = obter_lista_jogos(steam_id, api_key)
         
-        if game_list:
-            games_with_name = []
-            games_without_name = []
+        if lista_jogos:
+            jogos_com_nome = []
+            jogos_sem_nome = []
 
-            for game in game_list:
-                appid = game.get('appid', 'AppID not available')
-                playtime_minutes = game.get('playtime_forever', 0)
-                playtime_hours = playtime_minutes / 60
+            for jogo in lista_jogos:
+                appid = jogo.get('appid', 'AppID não disponível')
+                tempo_jogado_minutos = jogo.get('playtime_forever', 0)
+                tempo_jogado_horas = tempo_jogado_minutos / 60
 
-                game_name, img_url = get_game_name_and_image(appid, api_key)
+                nome_do_jogo, img_url = obter_nome_e_imagem_jogo(appid, api_key)
 
-                if game_name == 'Name not available' or game_name is None:
-                    games_without_name.append(('Name not available', appid, playtime_hours))
+                if nome_do_jogo == 'Nome não disponível' or nome_do_jogo is None:
+                    jogos_sem_nome.append(('Nome não disponível',appid, tempo_jogado_horas))
                 else:
-                    games_with_name.append((game_name, appid, playtime_hours))
+                    jogos_com_nome.append((nome_do_jogo, appid, tempo_jogado_horas))
 
-            games_with_name = sorted(games_with_name, key=lambda x: x[1], reverse=True)
+            jogos_com_nome = sorted(jogos_com_nome, key=lambda x: x[1], reverse=True)
 
-            show_results(personaname, personastate, avatar_url, games_with_name, current_game, last_game)
+            mostrar_resultados(personaname, personastate, avatar_url, jogos_com_nome,jogo_atual, ultimo_jogo)
            
 
-    def show_results(personaname, personastate, avatar_url, games_with_name, current_game, last_game):
-        result_window = tk.Toplevel(root)
-        result_window.title("Results")
-        screen_width = result_window.winfo_screenwidth()
-        screen_height = result_window.winfo_screenheight()
+    def mostrar_resultados(personaname, personastate, avatar_url, jogos_com_nome, jogo_atual, ultimo_jogo):
+        resultado_window = tk.Toplevel(root)
+        resultado_window.title("Resultados")
+        largura_tela = resultado_window.winfo_screenwidth()
+        altura_tela = resultado_window.winfo_screenheight()
 
-        # Set the dimensions of the main window
-        window_width = 500  # Adjust as needed
-        window_height = 600  # Adjust as needed
+        # Definir as dimensões da janela principal
+        largura_janela = 500  # Ajuste conforme necessário
+        altura_janela = 600  # Ajuste conforme necessário
 
-        # Calculate the position to center the window
-        x_pos = (screen_width - window_width) // 2
-        y_pos = (screen_height - window_height) // 2
+        # Calcular a posição para centralizar a janela
+        x_pos = (largura_tela - largura_janela) // 2
+        y_pos = (altura_tela - altura_janela) // 2
 
-        # Set the geometry of the main window
-        result_window.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
-        result_window.configure(bg='#262626')  # Dark background color
+        # Definir a geometria da janela principal
+        resultado_window.geometry(f"{largura_janela}x{altura_janela}+{x_pos}+{y_pos}")
+        resultado_window.configure(bg='#262626')  # Cor de fundo escura
 
-        # Center the photo
-        lbl_avatar = tk.Label(result_window)
+        # Centralizar foto
+        lbl_avatar = tk.Label(resultado_window)
         lbl_avatar.grid(row=0, column=0, columnspan=3, pady=10, sticky="n")
-        load_avatar(lbl_avatar, avatar_url)
+        carregar_avatar(lbl_avatar, avatar_url)
 
-        lbl_name = tk.Label(result_window, text=f"Personal Name: {personaname}", font=("Arial", 12, "bold"), bg='#262626', fg='white')
-        lbl_name.grid(row=1, column=0, columnspan=3, pady=5)
+        lbl_nome = tk.Label(resultado_window, text=f"Personal Name: {personaname}", font=("Arial", 12, "bold"), bg='#262626', fg='white')
+        lbl_nome.grid(row=1, column=0, columnspan=3, pady=5)
 
-        personastate_text = map_persona_state(personastate)
-        lbl_state = tk.Label(result_window, text=f"Persona State: {personastate_text}", font=("Arial", 12), bg='#262626', fg='white')
-        lbl_state.grid(row=2, column=1, columnspan=1, pady=5)
+        personastate_texto = mapear_persona_state(personastate)
+        lbl_estado = tk.Label(resultado_window, text=f"Persona State: {personastate_texto}", font=("Arial", 12), bg='#262626', fg='white')
+        lbl_estado.grid(row=2, column=1, columnspan=1, pady=5)
 
-        # Create state circle
+        # Criar círculo de estado
         if personastate == 0:
-            circle_color = "black"
+            cor_circulo = "black"
         elif personastate == 1:
-            circle_color = "green"
+            cor_circulo = "green"
         elif personastate == 2:
-            circle_color = "orange"
+            cor_circulo = "orange"
         elif personastate == 3:
-            circle_color = "pink"
+            cor_circulo = "pink"
         else:
-            circle_color = "white"
+            cor_circulo = "white"
 
-        state_circle = create_colored_circle(circle_color)
-        lbl_state_circle = tk.Label(result_window, image=state_circle, bg='#262626')
-        lbl_state_circle.photo = state_circle
-        lbl_state_circle.grid(row=2, column=2, pady=5, sticky="w")
+        circulo_estado = criar_circulo_cor(cor_circulo)
+        lbl_circulo_estado = tk.Label(resultado_window, image=circulo_estado, bg='#262626')
+        lbl_circulo_estado.photo = circulo_estado
+        lbl_circulo_estado.grid(row=2, column=2, pady=5, sticky="w")
 
-        lbl_current_game = tk.Label(result_window, text=f"Current Game: {current_game}", font=("Arial", 12), bg='#262626', fg='white')
-        lbl_current_game.grid(row=3, column=0, columnspan=3, pady=5)
+        lbl_jogo_atual = tk.Label(resultado_window, text=f"Jogo Atual: {jogo_atual}", font=("Arial", 12), bg='#262626', fg='white')
+        lbl_jogo_atual.grid(row=3, column=0, columnspan=3, pady=5)
 
-        lbl_last_game = tk.Label(result_window, text=f"Last Played Game: {last_game}", font=("Arial", 12), bg='#262626', fg='white')
-        lbl_last_game.grid(row=4, column=0, columnspan=3, pady=5)
+        lbl_ultimo_jogo = tk.Label(resultado_window, text=f"Último Jogo Jogado: {ultimo_jogo}", font=("Arial", 12), bg='#262626', fg='white')
+        lbl_ultimo_jogo.grid(row=4, column=0, columnspan=3, pady=5)
 
-        lbl_all_games = tk.Label(result_window, text="All Games", font=("Arial", 12, "underline"), bg='#262626', fg='white')
-        lbl_all_games.grid(row=5, column=0, columnspan=3, pady=5)
+        lbl_todos_jogos = tk.Label(resultado_window, text="Todos os Jogos", font=("Arial", 12, "underline"), bg='#262626', fg='white')
+        lbl_todos_jogos.grid(row=5, column=0, columnspan=3, pady=5)
 
-        text_all_games = scrolledtext.ScrolledText(result_window, width=60, height=15, bg='#333333', fg='white')
-        text_all_games.grid(row=6, column=0, columnspan=3, pady=5)
+        texto_todos_jogos = scrolledtext.ScrolledText(resultado_window, width=60, height=15, bg='#333333', fg='white')
+        texto_todos_jogos.grid(row=6, column=0, columnspan=3, pady=5)
 
-        all_games = sorted(games_with_name, key=lambda x: x[2], reverse=True)
+        todos_jogos = sorted(jogos_com_nome, key=lambda x: x[2], reverse=True)
 
-        for game in all_games:
-            if game[0] == 'Name not available' or game[0] == None or game[0] == "None":
-                game_name = f"Name not available (AppID: {game[1]})"
+        for jogo in todos_jogos:
+            if jogo[0] == 'Nome não disponível' or jogo[0] == None or jogo[0] == "None":
+                nome_jogo = f"Nome não disponível (AppID: {jogo[1]})"
             else:
-                game_name = f"{game[0]} (AppID: {game[1]})"
+                nome_jogo = f"{jogo[0]} (AppID: {jogo[1]})"
 
-            hours_played = game[2]
-            text_all_games.insert(tk.END, f"{game_name} - {hours_played:.2f} hours played\n")
+            horas_jogadas = jogo[2]
+            texto_todos_jogos.insert(tk.END, f"{nome_jogo} - {horas_jogadas:.2f} horas jogadas\n")
 
-        result_window.mainloop()
+        resultado_window.mainloop()
 
-    def load_avatar(label, avatar_url):
+
+
+    def carregar_avatar(label, avatar_url):
         response = requests.get(avatar_url)
         img_data = BytesIO(response.content)
         img = Image.open(img_data)
@@ -217,16 +221,17 @@ def create_main_screen():
         label.configure(image=photo)
         label.image = photo
 
+
     root = tk.Tk()
     root.title("Steam API Getter")
 
-    # Add a banner
-    banner_path = "steamAPIGetter/sample/asher-asmp-header.jpg"  # Replace with the actual path of the banner
+    # Adicione um banner
+    banner_path = "steamAPIGetter/sample/asher-asmp-header.jpg"  # Substitua pelo caminho real do banner
     if os.path.exists(banner_path):
         banner_img = Image.open(banner_path)
-        window_width = 500  # Adjust as needed
-        window_height = 600  # Adjust as needed
-        banner_img = banner_img.resize((window_width, 100))
+        largura_janela = 500  # Ajuste conforme necessário
+        altura_janela = 600  # Ajuste conforme necessário
+        banner_img = banner_img.resize((largura_janela, 100))
 
         banner_photo = ImageTk.PhotoImage(banner_img)
 
@@ -234,37 +239,36 @@ def create_main_screen():
         banner_label.photo = banner_photo
         banner_label.pack()
 
-    lbl_instruction = tk.Label(root, text="Steam ID:", font=("Arial", 12), bg='#333333', fg='white')
-    lbl_instruction.pack(pady=(20, 10))  # Added spacing above and below
+    lbl_instrucao = tk.Label(root, text="Insira seu Steam ID abaixo:", font=("Arial", 12), bg='#333333', fg='white')
+    lbl_instrucao.pack(pady=(20, 10))  # Adicionei um espaçamento acima e abaixo
 
     entry_steam_id = tk.Entry(root, width=30, font=("Arial", 12))
     entry_steam_id.pack(pady=10)
 
-    btn_get_info = tk.Button(root, text="Get Information", command=get_information, bg='#3498db', fg='white', font=("Arial", 12))
-    btn_get_info.pack(pady=10)
+    btn_obter_info = tk.Button(root, text="Obter Informações", command=obter_informacoes, bg='#3498db', fg='white', font=("Arial", 12))
+    btn_obter_info.pack(pady=10)
 
-    btn_linkedin = tk.Button(root, text="Dev LinkedIn", command=open_linkedin, bg='#0077b5', fg='white', font=("Arial", 10, "italic"))
+    btn_linkedin = tk.Button(root, text="Dev LinkedIn", command=abrir_linkedin, bg='#0077b5', fg='white', font=("Arial", 10, "italic"))
     btn_linkedin.pack(pady=5)
 
-    btn_github = tk.Button(root, text="Dev GitHub", command=open_github, bg='#333333', fg='white', font=("Arial", 10, "italic"))
+    btn_github = tk.Button(root, text="Dev GitHub", command=abrir_github, bg='#333333', fg='white', font=("Arial", 10, "italic"))
     btn_github.pack(pady=5)
     
-    window_width = 500  # Adjust as needed
-    window_height = 600  # Adjust as needed
-    # Get screen dimensions
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-    lbl_dev = tk.Label(root, text="@ashcrysis", font=("Arial", 12), bg='#262626', fg='white')
-    lbl_dev.pack(pady=(20, 10))  # Added spacing above and below
-    # Calculate the position to center the window
-    x_pos = (screen_width - window_width) // 2
-    y_pos = (screen_height - window_height) // 2
+    largura_janela = 500  # Ajuste conforme necessário
+    altura_janela = 600  # Ajuste conforme necessário
+    # Obtém as dimensões da tela
+    largura_tela = root.winfo_screenwidth()
+    altura_tela = root.winfo_screenheight()
 
-    # Set the geometry of the main window
-    root.geometry(f"{window_width}x{window_height}+{x_pos}+{y_pos}")
+    # Calcula a posição para centralizar a janela
+    x_pos = (largura_tela - largura_janela) // 2
+    y_pos = (altura_tela - altura_janela) // 2
 
-    root.configure(bg='#262626')  # Dark background color
+    # Define a geometria da janela principal
+    root.geometry(f"{largura_janela}x{altura_janela}+{x_pos}+{y_pos}")
+
+    root.configure(bg='#262626')  # Cor de fundo escura
     root.mainloop()
 
 if __name__ == "__main__":
-    create_main_screen()
+    criar_tela()
