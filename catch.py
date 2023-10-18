4f45d4216507d0518d49608044809f0693817fa5
asher gabriela <109311931+ashcrysis@users.noreply.github.com>
2023-10-18 15:43:39 -0300

    v-003 : Correcting display order

--- catch.py
+++ catch.py
@@ -86,14 +86,11 @@ def main():
     # Obter informações do jogador
     personaname, personastate = obter_informacoes_jogador(steam_id, api_key)
 
-
-   
-
     lista_jogos = obter_lista_jogos(steam_id, api_key)
 
     if lista_jogos:
         print('Lista de jogos:')
-        
+
         # Separar jogos com nome disponível e não disponível
         jogos_com_nome = []
         jogos_sem_nome = []
@@ -101,12 +98,12 @@ def main():
         for jogo in lista_jogos:
             appid = jogo.get('appid', 'AppID não disponível')
             tempo_jogado_minutos = jogo.get('playtime_forever', 0)
-            
+
             # Converter minutos para horas
             tempo_jogado_horas = tempo_jogado_minutos / 60
 
             nome_do_jogo, img_url = obter_nome_e_imagem_jogo(appid, api_key)
-            
+
             if nome_do_jogo == 'Nome não disponível':
                 jogos_sem_nome.append((appid, tempo_jogado_horas, img_url))
             else:
@@ -114,20 +111,23 @@ def main():
 
         # Ordenar jogos com nome disponível por horas de jogo em ordem decrescente
         jogos_com_nome = sorted(jogos_com_nome, key=lambda x: x[2], reverse=True)
+
         print("\n\n\n\n\n")
         print(f'Informações do jogador:')
         print(f'Personal Name: {personaname}')
         personastate = mapear_persona_state(personastate)
         print(f'Persona State: {personastate}\n')
+
         # Imprimir jogos com nome disponível
         for jogo_com_nome in jogos_com_nome:
             print(f"{jogo_com_nome[0]} (AppID: {jogo_com_nome[1]}) - {jogo_com_nome[2]:.2f} horas jogadas")
-            
 
-        # Imprimir jogos com nome não disponível por último
+        # Ordenar jogos sem nome por horas de jogo em ordem decrescente
+        jogos_sem_nome = sorted(jogos_sem_nome, key=lambda x: x[1], reverse=True)
+
+        # Imprimir jogos sem nome por último
         for jogo_sem_nome in jogos_sem_nome:
             print(f"Nome não disponível (AppID: {jogo_sem_nome[0]}) - {jogo_sem_nome[1]:.2f} horas jogadas")
-          
 
 if __name__ == "__main__":
     main()
